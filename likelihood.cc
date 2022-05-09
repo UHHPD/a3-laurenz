cecebf2e6f3bf1e926e33a5705af9e738bf0683a
Laurenz Kahl <102945441+LaurenzKahl@users.noreply.github.com>
2022-05-09 15:21:00 +0000

    Aufgabe 1 Komplett, Aufgabe 2a,b

--- likelihood.cc
+++ likelihood.cc
@@ -1,20 +1,38 @@
 #include <iostream>
 #include <fstream>
+#include <cmath>
 #include <vector>
+using namespace std;
 
+vector<int> daten;
+int zahl;
 
-double poisson(double mu, int k) {
-    return 0;
+int Construct(){
+  ifstream fin ("datensumme.txt");
+  for(int i=0; i<234;i++){
+    fin >> zahl;
+    daten.push_back(zahl);
+  }
+  fin.close();
 }
 
-int main() {
-    using namespace std;
+double prob(vector<int> daten, double mu){
+  double m = 1;
+  for(int k : daten){
+    m *= exp(-mu) * pow(mu, k) / tgamma(k + 1);
+  }
+  return m;
+}
 
-
-    ifstream fin("datensumme.txt");
-    int n_i;
-    for(int i = 0 ; i < 234 ; ++i) {
-        fin >> n_i;
-    }
-    fin.close();
+int main(){
+  ofstream fout ("likelihood.txt");
+  Construct();
+  cout << prob(daten, 3.11538) << endl;
+  double a = 0;
+  for (int i = 0; i<61;i++){
+    fout << a << " " << prob(daten, a) << endl;
+    cout << a << " " << prob(daten, a) << endl;
+    a += 0.1;
+  }
+  fout.close();
 }
