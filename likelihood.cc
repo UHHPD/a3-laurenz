31818decdad71ccb343c1396d22df99cb680f16d
Laurenz Kahl <102945441+LaurenzKahl@users.noreply.github.com>
2022-05-16 15:18:19 +0000

    2e

--- likelihood.cc
+++ likelihood.cc
@@ -24,19 +24,30 @@ double prob(vector<int> daten, double mu){
   return m;
 }
 
+double prob2(vector<int> daten){
+  double m = 1;
+  for(int k : daten){
+    m *= exp(-k) * pow(k, k) / tgamma(k + 1);
+  }
+  return m;
+}
+
 int main(){
   ofstream fout ("likelihood.txt");  
   ofstream fout2 ("nll.txt");
   ofstream fout3 ("deltanll.txt");
   Construct();
   cout << prob(daten, 3.11538) << endl;
-  double a = 0;
+  double a = 0,b;
   for (int i = 0; i<61;i++){
     fout << a << " " << prob(daten, a) << endl;
     fout2 << a << " " << -2*log(prob(daten, a)) << endl;
     fout3 << a << " " << -2*log(prob(daten, a))-2*log(prob(daten, 3.11538)) << endl;
     a += 0.1;
   }
+  b = -2*log(prob(daten, 3.11538)/prob2(daten));
+  cout << -2*log(prob(daten, 3.11538)/prob2(daten)) << endl;
+  cout << (b-233)/sqrt(2*233) << endl;
   fout.close();
   fout2.close();
   fout3.close();
